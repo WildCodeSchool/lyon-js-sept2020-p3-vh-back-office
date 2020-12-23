fee98dadae19c600c9e40d54740399c1121cbc39
Aymeric <aymeric.bouault@gmail.com>
2020-12-23 13:34:42 +0100

    méthodes PostList, PostEdit, PostCreate

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import * as React from "react";
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
+import { PostList, PostEdit, PostCreate } from './posts';
+import { UserList } from './users';
 import jsonServerProvider from "ra-data-json-server";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 const App = () => (
-      <Admin dataProvider={dataProvider}>
-          <Resource name="users" list={ListGuesser} />
-      </Admin>
-  );
+  <Admin dataProvider={dataProvider}>
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
+    <Resource name="users" list={UserList} />
+  </Admin>
+);
 
 export default App;
---
+++ src/components/posts.js
@@ -0,0 +1,52 @@
+import * as React from "react";
+import {
+    List,
+    Datagrid,
+    TextField,
+    ReferenceField,
+    EditButton,
+    Edit,
+    SimpleForm,
+    ReferenceInput,
+    SelectInput,
+    TextInput,
+    Create,
+} from 'react-admin';
+
+export const PostList = props => (
+    <List {...props}>
+            <Datagrid>
+            <TextField source="id" />
+            <ReferenceField source="userId" reference="users">
+                <TextField source="name" />
+            </ReferenceField>
+            <TextField source="title" />
+            <EditButton />
+        </Datagrid>
+    </List>
+);
+
+export const PostEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+        <TextInput disabled source="id" />
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = props => (
+        <Create {...props}>
+            <SimpleForm>
+                <ReferenceInput source="userId" reference="users">
+                    <SelectInput optionText="name" />
+                </ReferenceInput>
+                <TextInput source="title" />
+                <TextInput multiline source="body" />
+            </SimpleForm>
+        </Create>
+    );
