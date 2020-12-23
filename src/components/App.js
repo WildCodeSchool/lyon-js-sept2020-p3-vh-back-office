71187e78214e7e17588c11e16c0e131e85fedc7a
Aymeric <aymeric.bouault@gmail.com>
2020-12-23 13:04:25 +0100

    affichage des utilisateurs

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 import * as React from "react";
-import { Admin } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import jsonServerProvider from "ra-data-json-server";
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-const App = () => <Admin dataProvider={dataProvider} />;
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+const App = () => (
+      <Admin dataProvider={dataProvider}>
+          <Resource name="users" list={ListGuesser} />
+      </Admin>
+  );
 
 export default App;
