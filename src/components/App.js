aba5533e3ba3f9762664b3e87ab21dd0c508588d
Thibault Verin <thibaultverin@hotmail.com>
2021-01-19 13:23:52 +0100

    Add meta title to the back office

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Helmet } from 'react-helmet';
 import { Admin, Resource } from 'react-admin';
 import EmailIcon from '@material-ui/icons/Email';
 import PersonIcon from '@material-ui/icons/Person';
@@ -13,45 +14,50 @@ import { sponsorsList, createSponsor, showSponsors } from './sponsors/Sponsors';
 import { wineList, createWine, showWine, wineEdit } from './Wine/Wine';
 
 const App = () => (
-  <Admin
-    dataProvider={dataProvider}
-    dashboard={Dashboard}
-    authProvider={authProvider}
-  >
-    <Resource
-      options={{ label: 'Messages' }}
-      name="contact"
-      list={messageList}
-      create={createMessage}
-      show={showMessage}
-      icon={EmailIcon}
-    />
-    <Resource
-      options={{ label: 'Users' }}
-      name="users"
-      list={userList}
-      create={createUser}
-      show={showUser}
-      edit={userEdit}
-      icon={PersonIcon}
-    />
-    <Resource
-      name="sponsors"
-      list={sponsorsList}
-      create={createSponsor}
-      show={showSponsors}
-      icon={BusinessCenterIcon}
-    />
-    <Resource
-      options={{ label: 'Wines' }}
-      name="products"
-      list={wineList}
-      create={createWine}
-      show={showWine}
-      edit={wineEdit}
-      icon={LocalBarIcon}
-    />
-  </Admin>
+  <>
+    <Helmet>
+      <title>Hypnose & Vins</title>
+    </Helmet>
+    <Admin
+      dataProvider={dataProvider}
+      dashboard={Dashboard}
+      authProvider={authProvider}
+    >
+      <Resource
+        options={{ label: 'Messages' }}
+        name="contact"
+        list={messageList}
+        create={createMessage}
+        show={showMessage}
+        icon={EmailIcon}
+      />
+      <Resource
+        options={{ label: 'Users' }}
+        name="users"
+        list={userList}
+        create={createUser}
+        show={showUser}
+        edit={userEdit}
+        icon={PersonIcon}
+      />
+      <Resource
+        name="sponsors"
+        list={sponsorsList}
+        create={createSponsor}
+        show={showSponsors}
+        icon={BusinessCenterIcon}
+      />
+      <Resource
+        options={{ label: 'Wines' }}
+        name="products"
+        list={wineList}
+        create={createWine}
+        show={showWine}
+        edit={wineEdit}
+        icon={LocalBarIcon}
+      />
+    </Admin>
+  </>
 );
 
 export default App;
