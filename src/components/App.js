b9044be26e69e133fdef57a814b4707c8e4bb0b7
BrieucQ <brieucquertier73@gmail.com>
2021-01-19 09:31:41 +0100

    Route users-office done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { Admin, Resource } from 'react-admin';
 import EmailIcon from '@material-ui/icons/Email';
+import PersonIcon from '@material-ui/icons/Person';
 import Dashboard from './Dashboard';
 import authProvider from './authProvider';
 import dataProvider from './dataProvider';
 import { messageList, createMessage, showMessage } from './contact/Contact';
+import { userList, showUser, createUser, userEdit } from './user/user';
 
 const App = () => (
   <Admin
@@ -19,6 +21,15 @@ const App = () => (
       create={createMessage}
       show={showMessage}
       icon={EmailIcon}
+    />
+    <Resource
+      options={{ label: 'Users' }}
+      name="users"
+      list={userList}
+      create={createUser}
+      show={showUser}
+      edit={userEdit}
+      icon={PersonIcon}
     />
   </Admin>
 );
